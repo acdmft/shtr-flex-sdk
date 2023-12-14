b8f69f3b53f40174a40a623af7d4694ba734fb7b
gilert <crankyfox@protonmail.com>
2023-12-14 21:56:43 +0100

    feat: use flex-sdk method to query listings

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -1,39 +1,59 @@
-import React from 'react';
-import { Button, BreadcrumbLink,Stack } from '@chakra-ui/react'
-import { Link as ChakraLink} from '@chakra-ui/react'
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Button, BreadcrumbLink, Stack } from "@chakra-ui/react";
+import { Link as ChakraLink } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+const sharetribeSdk = require('sharetribe-flex-sdk');
+
 export default function Listings() {
-    return (
-        // <>
-        // <h2>Listings</h2>
+  const [data, setData] = useState([]);
+  useEffect(()=> {
+    const sdk = sharetribeSdk.createInstance({
+      clientId: process.env.REACT_APP_SHARETRIBE_SDK_CLIENT_ID,
+    });
+    sdk.listings
+      .query({ perPage: 5 })
+      .then((res) => {
+        setData(res.data.data)
+        // Print listing titles
+        // listings = res.data.data;
+        console.log('listings', data);
+        res.data.data.forEach((listing) => {
+          console.log(`Listing: ${listing.attributes.title}`);
+        });
+      })
+      .catch((res) => {
+        // An error occurred
+        console.log(
+          `Request failed with status: ${res.status} ${res.statusText}`
+        );
+      });
+  }, [])
+  // let listings = [];//["1","3","33"];
+  return (
+    <Stack direction="row" spacing={4} align="center">
+      {data.map((item, index) => (
+        <p>item: 
         
-        
-        //     <nav>
-        //         <ul>
-        //         <li>
-        //             <Link to="/listings/111">111</Link>
-        //         </li>
-        //         <li>
-        //             <Link to="/listings/222">222</Link>
-        //         </li>
-        //         <li>
-        //             <Link to="/listings/333">333</Link>
-        //         </li>
-        //         </ul>
-        //     </nav>
-            
-            
-        // </>
-    <Stack direction='row' spacing={4} align='center'>
-        <Button colorScheme='teal' variant='solid'>
-            <ChakraLink as={Link} to="/listings/111">111</ChakraLink>
-        </Button>
-        <Button colorScheme='teal' variant='outline'>
-            <ChakraLink as={Link} to="/listings/222">222</ChakraLink>
-        </Button>
-        <Button colorScheme='teal' variant='ghost'>
-            <ChakraLink as={Link} to="/listings/333">333</ChakraLink>
-        </Button>   
+      <Button colorScheme="teal" variant="solid">
+        <ChakraLink as={Link} to="/listings/111">
+          {item.attributes.title} 
+          
+        {index}
+        </ChakraLink>
+      </Button>
+        </p>
+      ))
+    }
+      <Button colorScheme="teal" variant="outline">
+        <ChakraLink as={Link} to="/listings/222">
+          222
+        </ChakraLink>
+      </Button>
+      <Button colorScheme="teal" variant="ghost">
+        <ChakraLink as={Link} to="/listings/333">
+          333
+        </ChakraLink>
+      </Button>
     </Stack>
-    )
+  );
 }
