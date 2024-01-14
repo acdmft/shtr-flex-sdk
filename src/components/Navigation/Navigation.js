ed3fb7809bbdcffa064e751d37581e823fab624f
gilert <crankyfox@protonmail.com>
2024-01-14 19:46:14 +0100

    feat: add state in the component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,31 +1,50 @@
 import { Link } from "react-router-dom";
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbSeparator,
-  } from '@chakra-ui/react'
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+} from "@chakra-ui/react";
+import { useSelector } from "react-redux";
 
 export default function NavigationMenu() {
-    return (
-        <Breadcrumb>
-            <BreadcrumbItem>
-            <BreadcrumbLink as={Link} to='/'>Home</BreadcrumbLink>
-            </BreadcrumbItem>
-        
-            <BreadcrumbItem>
-            <BreadcrumbLink as={Link} to='/listings'>Listings</BreadcrumbLink>
-            </BreadcrumbItem>
-        
-            <BreadcrumbItem isCurrentPage>
-            <BreadcrumbLink as={Link} to='/about'>About</BreadcrumbLink>
-            <BreadcrumbSeparator />
-            </BreadcrumbItem>
+  const isAuthenticated = useSelector(state => state.user.isAuthenticated);
+  console.log('isAuthenticated ', isAuthenticated)
+  return (
+    <Breadcrumb>
+      <BreadcrumbItem>
+        <BreadcrumbLink as={Link} to="/">
+          Home
+        </BreadcrumbLink>
+      </BreadcrumbItem>
 
-            <BreadcrumbItem isCurrentPage>
-            <BreadcrumbLink as={Link} to='/connect'>Connect</BreadcrumbLink>
-            <BreadcrumbSeparator />
-            </BreadcrumbItem>
-        </Breadcrumb>
-    )
+      <BreadcrumbItem>
+        <BreadcrumbLink as={Link} to="/listings">
+          Listings
+        </BreadcrumbLink>
+      </BreadcrumbItem>
+
+      <BreadcrumbItem isCurrentPage>
+        <BreadcrumbLink as={Link} to="/about">
+          About
+        </BreadcrumbLink>
+        <BreadcrumbSeparator />
+      </BreadcrumbItem>
+      {isAuthenticated ? (
+        <BreadcrumbItem isCurrentPage>
+          <BreadcrumbLink as={Link} to="/">
+            Profile
+          </BreadcrumbLink>
+          <BreadcrumbSeparator />
+        </BreadcrumbItem>
+      ) : (
+        <BreadcrumbItem isCurrentPage>
+          <BreadcrumbLink as={Link} to="/connect">
+            Connect
+          </BreadcrumbLink>
+          <BreadcrumbSeparator />
+        </BreadcrumbItem>
+      )}
+    </Breadcrumb>
+  );
 }
