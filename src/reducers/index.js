c8a30de3b224d9dda4582d9da682b396e25ab384
gilert <crankyfox@protonmail.com>
2024-01-14 19:43:21 +0100

    fix: change path to the reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import userReducer from './userReducer';
+import userReducer from '../ducks/auth.duck.js';
 
 const rootReducer = combineReducers({
     user: userReducer,
